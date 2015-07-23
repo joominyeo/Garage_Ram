983942dfb0745cec3ff517009145cf888d54aa20
Joomin Yeo <caiiyeo97@live.com>
2015-07-23 11:34:13 -0700

    Central Update
    Divided ino in to respective folders.
    Combined Anode, Photo, and Prox into Garage_Ram.ino

--- Garage_Ram.ino
+++ Garage_Ram.ino
@@ -1,24 +1,44 @@
 /*
  * Written by: Joo Min (Cai) Yeo
  * Last Edit: 201507152258 PST
+ * Location: Fullerton, California, United States
  *
- * Project Description: This project was created to prevent the cars from ramming into
- *                      garage walls when parking - inspired by watching my mother and
- *                      father scream after punching a hole in the wall.
+ * Project Description: This project was created to prevent my parent's cars from ramming
+ *                      into garage walls when parking - inspired by watching my mother
+ *                      and father scream after punching a hole in the wall with their car.
  *
  * Project Cost: $29.94 USD
  *
  * Analog write reference, https://www.arduino.cc/en/Reference/analogWrite
  * 7-Seg display code from Rui Santos, http://randomnerdtutorials.com/arduino-temperature-displayed-on-4-digit-7-segment/
  * Wiring schematics, http://www.instructables.com/id/Arduino-4-digit-7-segment-display/
+ *
+ * 1) Sound Sensor(SON) picks up the sound from Analog 0
+ * 2) Photoresistor Sensor(PHR) picks up the light fron the headlights only at night from Analog 1
+ * 3) Sharp IR Sensor(SIR) measure the distance from 20 - 150 cm from Analog 2
+ * 4) Distance 80 ~ 150 cm lights the Green LED's at Digital 2
+ * 5) Distance 30 ~ 80 cm lights the Yellow LED's at Digital 3
+ * 6) 7 Segment 4 Digit Anode displays(SFD) the distance as it is updated by the SIR
+ * 7) IR Obstacle Avoidance Module(OAM) measure the distance from 3 - 30 cm from Analog 3
+ * 8) Distance 2 ~ 30 cm lights the Red LED's at Digital 4
+ *
 */
 
+int SONpin = 0;
+int SON = 0;
+int PHRpin = 1;
+int PHR = 0;
+int SIRpin = 2;
+int SIR = 0;
+int OAMpin = 3;
+int OAM = 0;
+int dist = 650; //in centimetres
+
 const int digitPins[4] = {
-  4,5,6,7};                 //4 common anode pins of the display
-const int clockPin = 11;    //74HC595 Pin 11 
-const int latchPin = 12;    //74HC595 Pin 12
-const int dataPin = 13;     //74HC595 Pin 14
-const int tempPin = A0;     //temperature sensor pin
+  5,6,7,8};                 //4 common anode pins of the display
+const int clockPin = 9;    //74HC595 Pin 9
+const int latchPin = 10;    //74HC595 Pin 10
+const int dataPin = 11;     //74HC595 Pin 11
 const byte digit[10] =      //seven segment digits in bits
 {
   B00111111, //0
@@ -32,41 +52,74 @@ const byte digit[10] =      //seven segment digits in bits
   B01111111, //8
   B01101111  //9
 };
-int digitBuffer[4] = {
-  0};
+int digitBuffer[4] = {0};
 int digitScan = 0, flag=0,  soft_scaler = 0;
-;
-float tempK, tempC, tempF, temp;
- 
-void setup(){                
-  for(int i=0;i<4;i++)
-  {
+
+void setup() {
+  delay(100);
+  Serial.begin(9600);
+  delay(100);
+  Serial.println("Initiating Garage_Ram");
+  
+  for(int i=0;i<4;i++) {
     pinMode(digitPins[i],OUTPUT);
   }
-  pinMode(tempPin, INPUT);
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);  
-  pinMode(tempPin, INPUT);
+  pinMode(dataPin, OUTPUT);
+  
+  for (int x = 0; x < 10; x++) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println(" 100%");
+  delay(1000);
+  Serial.println("Preventing Garage Ramming!");
+  delay(100);
 }
 
-void loop(){ 
-  updateDisp();
-  delay(2);
+void loop() {
+  if(dist > 150 && (SON > 400 || PHR < 500)) { //SOUND INPUT FROM GARAGE & LUX FROM HEADLIGHTS
+    //flash all three LED's
+    updateDist(0);
+  }
+  else{
+    if(dist < 150 && dist > 70) {
+      /*GREEN LED's
+      */
+      updateDist(0);
+      updateDisp;
+      delay(2);
+    }
+    else if(dist < 90 && dist > 30) {
+      /*YELLOW LED's
+      */
+      updateDist(0);
+      updateDisp;
+      delay(2);
+    }
+    else if(dist < 30) {
+      /*RED LED's
+      */
+      updateDist(1);
+      updateDisp;
+      delay(2);
+    }
+  }
 }
- 
-//writes the temperature on display
+
+void updateDist(int mode) {
+  if (mode == 0) dist = 20798 * pow(analogRead(SIR),-1.109);
+  else {
+    //EQUATION FOR OAM - NEEDS TO BE CALIBRATED
+  }
+}
+
 void updateDisp(){
-  tempK = (((analogRead(tempPin)/ 1023.0) * 5.0) * 100.0);  
-  //Converts Kelvin to Celsius minus 2.5 degrees error
-  tempC = tempK - 273.0;   
-  tempF = ((tempK - 2.5) * 9 / 5) - 459.67;
-  //Celsius temperature display
-  tempC = int(tempC*100);
-  digitBuffer[3] = int(tempC)/1000;
-  digitBuffer[2] = (int(tempC)%1000)/100;
-  digitBuffer[1] = (int(tempC)%100)/10;
-  digitBuffer[0] = (int(tempC)%100)%10;
+  digitBuffer[3] = int(dist * 100)/1000;
+  digitBuffer[2] = (int(dist * 100)%1000)/100;
+  digitBuffer[1] = (int(dist * 100)%100)/10;
+  digitBuffer[0] = (int(dist * 100)%100)%10;
   
   for(byte j=0; j<4; j++)  
     digitalWrite(digitPins[j], LOW);
@@ -79,12 +132,13 @@ void updateDisp(){
   digitalWrite(digitPins[digitScan], HIGH); 
  
   digitalWrite(latchPin, LOW);  
+  /*
   if(digitScan==2)
     shiftOut(dataPin, clockPin, MSBFIRST, ~(digit[digitBuffer[digitScan]] | B10000000)); //print the decimal point on the 3rd digit
     // NO NEED FOR DECIMAL POINT b/c dist IN CENTIMETRES
   else
     shiftOut(dataPin, clockPin, MSBFIRST, ~digit[digitBuffer[digitScan]]);
- 
+   */
   digitalWrite(latchPin, HIGH);
   digitScan++;
   if(digitScan>3) digitScan=0; 
--- Sharp_prox_sensor/Sharp_prox_sensor.ino
+++ Sharp_prox_sensor/Sharp_prox_sensor.ino
@@ -3,11 +3,20 @@ int proxSensor = 0;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Start countdown:");
+  Serial.println("3");
+  delay(1000);
+  Serial.println("2");
+  delay(1000);
+  Serial.println("1");
+  delay(1000);
+  Serial.println("GO");
+  delay(1000);
 }
 
 void loop() {
   int raw = analogRead(proxSensor);
-  Serial.println(raw);
-  delay(100);
+  int dist = 20798 * pow(raw,-1.109);
+  Serial.println(dist);
+  delay(1000);
 }
-
